eac232e20acf5f64fa704c0fcd05e29f9e301072
MSRbuddy <meghanadasari222@gmail.com>
2023-03-11 15:59:53 +0530

    Address-Book System using LINQ ---> UC1-UC3 are added!

---
+++ LINQ-UC1-UC3/TestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+using LINQ_AddressBook;
+
+namespace ContactManagerTesting
+{
+    [TestClass]
+    public class AddressBookTesting
+    {
+        DataTableManager dataTableManger;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            dataTableManger = new DataTableManager();
+        }
+
+        [TestMethod]
+        [TestCategory("Insert Values in Data Table")]
+        public void GivenInsertValues_returnInteger()
+        {
+            int expected = 2;
+            int actual = dataTableManger.AddValues();
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}

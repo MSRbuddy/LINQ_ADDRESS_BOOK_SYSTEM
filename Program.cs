885e3e0b3e80d86851e6991e4da5b52651290060
MSRbuddy <meghanadasari222@gmail.com>
2023-03-11 15:56:39 +0530

    Welcome to Address-Book System using LINQ!

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address-Book System using LINQ!");
+        }
+    }
+}
